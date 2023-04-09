ad6fcbed258ed6a23627eb44aecf74bfc0b07707
Hailey <haileyacarlson@gmail.com>
2023-04-08 19:07:25 -0700

    fetch data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,39 @@
-var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form');
+var historyEl = document.querySelector('#history');
+var cityEL = document.querySelector('#city');
+var currentPicEl = document.querySelector('#current-pic');
+var currentTemp = document.querySelector('#temp');
+var currentWind = document.querySelector('#wind');
+var currentHummidity = document.querySelector('#hummidity');
 
-function getParams() {
-    document.getElementById()
-    
-}
+let searchHistory = JSON.parse(localStorage.getItem("search-input"))
 
-function searchApi(query, format) {
-    let locQueryUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid='
-    if (format) {
-        locQueryUrl = ''
-    }
+const APIKey = "68a01bedfe0281ebdf08d0137195550e"
+
+function searchWeather(city) {
+    let locQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIKey
+    fetch(locQueryUrl).then(res=>res.json()).then(data=>{
+      console.log(data)
+
+    }).catch(e=>console.error(e))
+   
 }
 
 function handleSearchFormSubmit(event) {
+  alert("yes")
     event.preventDefault();
   
-    var searchInputVal = document.querySelector('#search-input').value;
+    var cityName = document.querySelector('#search-input').value;
   
-    if (!searchInputVal) {
-      console.error('You need a search input value!');
+    if (!cityName) {
+      console.error('You need to enter a city!');
       return;
     }
   
-    searchApi(searchInputVal);
+    searchWeather(cityName);
   }
   
   searchFormEl.addEventListener('submit', handleSearchFormSubmit);
   
-  getParams();
+  
   
